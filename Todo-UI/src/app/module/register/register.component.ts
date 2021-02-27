abcd9e742601af5da3ef0ce5502a45d7d13428f8
Kuminga Makashi <kumingam@marquisfinance.co.za>
2021-02-28 00:28:54 +0200

    new update

--- Todo-UI/src/app/module/register/register.component.ts
+++ Todo-UI/src/app/module/register/register.component.ts
@@ -12,18 +12,15 @@ import {UUID} from 'angular2-uuid';
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   showPassword: boolean ;
-  showVerifyPassword: boolean ;
   constructor(private formBuilder: FormBuilder,
               private userService: UserService
   ) { }
 
   ngOnInit(): void {
-    console.log('ngOnInit', UUID.UUID());
     this.registerForm = this.formBuilder.group({
       name: ['', [Validators.required, Validators.maxLength(55)]],
       password: ['', [Validators.required,  Validators.maxLength(30)]],
       email: ['', [Validators.required, Validators.maxLength(60)]],
-      preferredComm: ['', [Validators.required]]
     });
   }
 
@@ -33,18 +30,11 @@ export class RegisterComponent implements OnInit {
     userModel.email = this.registerForm.value.email;
     userModel.password = this.registerForm.value.password;
     userModel.id = UUID.UUID().toString();
-    console.log('userModel xxxxxxxxxxxxxxxxxxxxxxx', userModel);
-    // { username: this.loginFormWithUsername.value.username, password: this.loginFormWithUsername.value.password, preferredComm: this.loginFormWithUsername.value.preferredComm }
     this.userService.register(userModel)
           .subscribe((data) => {
-            console.log('data ***', data);
-            // if (data['status'] == "Success") {
-            //   this.messageService.add({ sticky: true, severity: 'success', summary: 'OTP', detail: 'OTP Sent successfully' });
-            //   this.activeIndex = 2;
-            // }
-            // else {
-            //   this.messageService.add({ sticky: true, severity: 'error', summary: 'OTP Failed', detail: data['message'] });
-            // }
+            if (data) {
+              alert('Successfully registered');
+            }
     });
   }
 }
