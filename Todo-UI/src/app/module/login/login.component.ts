a413f2dc0e62f3e4598f4f9a5a024bef8607b482
Kuminga Makashi <kumingam@marquisfinance.co.za>
2021-02-28 00:31:26 +0200

    update

--- Todo-UI/src/app/module/login/login.component.ts
+++ Todo-UI/src/app/module/login/login.component.ts
@@ -28,17 +28,9 @@ export class LoginComponent implements OnInit {
     const userModel = new UserModel();
     userModel.email = this.loginForm.value.email;
     userModel.password = this.loginForm.value.password;
-    console.log('userModel xxxxxxxxxxxxxxxxxxxxxxx', userModel);
     this.userService.login({ email: this.loginForm.value.email, password: this.loginForm.value.password})
       .subscribe((data) => {
         console.log('data ***', data);
-        // if (data['status'] == "Success") {
-        //   this.messageService.add({ sticky: true, severity: 'success', summary: 'OTP', detail: 'OTP Sent successfully' });
-        //   this.activeIndex = 2;
-        // }
-        // else {
-        //   this.messageService.add({ sticky: true, severity: 'error', summary: 'OTP Failed', detail: data['message'] });
-        // }
       });
   }
 }
