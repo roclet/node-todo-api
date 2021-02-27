7f7091d60433f9da9f3c1c0702ef20260167db22
Kuminga Makashi <kumingam@marquisfinance.co.za>
2021-02-27 16:19:02 +0200

    allow cors

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const mount = require("koa-mount");
 const session = require("koa-session");
 const jwt = require('koa-jwt');
 const database = require("./database");
+const cors = require('@koa/cors');
+// const cors = require('cors');
 // Router imports
 const todoRoutes = require("./routes/todo");
 const authRoutes = require("./routes/auth");
@@ -16,8 +18,15 @@ const main = async () => {
   // Create the database tables
   await database.sequelize.sync();
   const app = new Koa();
-  
+  app.use(cors());
+  // app.use((req, res, next) => {
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.header("Access-Control-Allow-Methods", "GET , PUT , POST , DELETE");
+  //   res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
+  //   next(); // Important
+  // })
   app.use(session({key: 'koa.sess'}, app));
+  // app.use(cors());
   app.use(convert(koaBetterBody({fields: "body"})));
   
   app.use(mount(authRoutes.allowedMethods()));
